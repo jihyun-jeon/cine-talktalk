3d723d540ccefbeb841d981cdd461a80c619214b
jihyun-jeon <jhplus13@naver.com>
2025-04-15 01:06:15 +0900

    chore

--- apps/movie-app/src/App.tsx
+++ apps/movie-app/src/App.tsx
@@ -5,14 +5,11 @@ import { AuthContextProvider } from '@/context/AuthContext';
 import SpinnerPortal from '@repo/ui/Spinner';
 import AuthErrorBoundary from '@/context/AuthErrorBoundary';
 
-import { Button } from '@repo/ui/button';
-
 function App() {
   return (
     <AuthErrorBoundary>
       <Suspense fallback={<SpinnerPortal />}>
         <AuthContextProvider>
-          <Button appName="movie-app">클릭!!</Button>
           <RouterProvider router={router} />
         </AuthContextProvider>
       </Suspense>
