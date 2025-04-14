2276918eba3ff55eb83bbe0ae08c21c37be33eff
jihyun-jeon <jhplus13@naver.com>
2025-04-14 23:28:10 +0900

    refac: turbo-repo 활용한 모노레포 구축

--- apps/movie-app/src/App.tsx
+++ apps/movie-app/src/App.tsx
@@ -2,14 +2,17 @@ import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/router';
 import { AuthContextProvider } from '@/context/AuthContext';
-import SpinnerPortal from '@/components/Spinner';
+import SpinnerPortal from '@repo/ui/Spinner';
 import AuthErrorBoundary from '@/context/AuthErrorBoundary';
+
+import { Button } from '@repo/ui/button';
 
 function App() {
   return (
     <AuthErrorBoundary>
       <Suspense fallback={<SpinnerPortal />}>
         <AuthContextProvider>
+          <Button appName="movie-app">클릭!</Button>
           <RouterProvider router={router} />
         </AuthContextProvider>
       </Suspense>
