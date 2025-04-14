4a11c0fee7e5172d7e4f61f80ddd5961395526e7
jihyun-jeon <jhplus13@naver.com>
2025-04-15 01:30:52 +0900

    reaf: 환경변수가 DefinePlugin을 통해 Webpack 빌드에 반영되도록 개선

--- apps/movie-app/webpack.prod.js
+++ apps/movie-app/webpack.prod.js
@@ -3,6 +3,7 @@
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'production', // 프로덕션 모드 설정
@@ -10,7 +11,14 @@ module.exports = merge(common, {
   output: {
     publicPath: process.env.REACT_APP_BASENAME + '/',
   },
-  plugins: [...[new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })]].filter(Boolean),
+  plugins: [
+    ...[
+      new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_BASENAME': JSON.stringify(process.env.REACT_APP_BASENAME),
+      }),
+    ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
